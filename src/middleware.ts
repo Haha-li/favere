c567631213e6d56e826d34ab500190971d584f05
Haha-li <18237151969@163.com>
2026-06-04 11:44:51 +0800

    优化

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,25 @@ import { defineMiddleware } from 'astro:middleware';
 export const onRequest = defineMiddleware(async (context, next) => {
   if (import.meta.env.PROD && context.url.pathname.startsWith('/api/keystatic/')) {
     const { env } = await import('cloudflare:workers');
+    const locals = context.locals as typeof context.locals & {
+      runtime?: Record<string, unknown>;
+    };
 
-    Object.defineProperty(context.locals, 'runtime', {
-      configurable: true,
-      enumerable: false,
-      value: {
-        env,
-      },
-    });
+    if (locals.runtime && typeof locals.runtime === 'object') {
+      Object.defineProperty(locals.runtime, 'env', {
+        configurable: true,
+        enumerable: true,
+        value: env,
+      });
+    } else {
+      Object.defineProperty(locals, 'runtime', {
+        configurable: true,
+        enumerable: false,
+        value: {
+          env,
+        },
+      });
+    }
   }
 
   return next();
